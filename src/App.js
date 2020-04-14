3d3b33dc56235af2b4c0e00c213a5ada23ed0cde
Ravenor222 <ndanvers222@gmail.com>
2020-04-14 10:32:11 -0700

    successfully receiving from server w/out cors error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+
 
 function App() {
+
+  useEffect(()=>{
+    axios.get('http://localhost:3001/').then((res)=> console.log(res));
+  })
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -24,3 +34,8 @@ function App() {
 }
 
 export default App;
+
+
+// the back half of the interview was a React exercise where he gave me an API call for best buy data of products and I had to build a live search
+//  for it on the spot. so i had to call the API, set the data to state, create a filtering function, create an input, make it a controlled component
+//  , and useEffect to tie it all together. It was pretty fun and really glad I did react for Book People and I could use those skills
